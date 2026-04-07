d68555a5ba71293fe43befd814a353a4883fe295
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-04-07 14:13:36 +0100

    fileUploaderController.js: Wrote asyncHandler and homeGet

---
+++ controllers/fileUploaderController.js
@@ -0,0 +1,18 @@
+// need db somehow
+const { validationResult, matchedData } = require("express-validator");
+const validateUser = require("../validators/userValidator");
+const bcrypt = require("bcryptjs");
+
+function asyncHandler(fn) {
+  return function (req, res, next) {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
+
+async function homeGet(req, res) {
+    res.render("index");
+}
+
+module.exports = {
+    homeGet
+}
