e4ef9f3d3733e7601d6a7934fefd410fb9865da9
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-04-08 21:47:20 +0100

    passport.js: Wrote strategy for passport and functions for serializing and deserializing users

---
+++ passport/passport.js
@@ -0,0 +1,44 @@
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const db = require("../db/queries");
+const bcrypt = require("bcryptjs");
+
+passport.use("user-local",
+    new LocalStrategy(async (username, password, done) => {
+        try {
+            const rows = await db.findUserByUsername({ username });
+            const user = rows[0];
+
+            if (!user) {
+                return done(null, false, { message: "Username not found" });
+            }
+
+            const match = await bcrypt.compare(password, user.password);
+            if (!match) {
+                return done(null, false, { message: "Incorrect password" });
+            }
+
+            return done(null, user);
+        } catch(err) {
+            return done(err);
+        }
+    })
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await db.findUserById({ id });
+
+        done(null, user);
+    } catch(err) {
+        done(err);
+    }
+});
+
+module.exports = {
+    passport
+}
