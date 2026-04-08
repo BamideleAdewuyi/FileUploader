963b30d1319e4a1d9469a55b83f567e000e584a6
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-04-08 20:22:16 +0100

    userValidator.js: Added check for username being in use

--- validators/userValidator.js
+++ validators/userValidator.js
@@ -1,15 +1,23 @@
 const { body, validationResult, matchedData } = require("express-validator");
+const db = require("../db/queries");
 
 const alphaErr = "must only contain letters and spaces";
 const lengthErr = "cannot be blank";
 const usernameErr = "can only contain letters and numbers";
 const passwordErr = "must be 3 characters";
 const confirmPasswordErr = "Passwords do not match";
+const usernameInUseErr = "Username already in use";
 
 const validateUser = [
     body("username").trim()
         .matches(/^[a-zA-Z0-9]+$/, 'i').withMessage(`Username ${usernameErr}`)
-        .isLength({ min: 1 }).withMessage(`First name ${lengthErr}`),
+        .isLength({ min: 1 }).withMessage(`First name ${lengthErr}`)
+        .custom(async value => {
+                    const user = await db.findUserByUsername({ username: value });
+                    if (user) {
+                        throw new Error(usernameInUseErr);
+                    }
+                }),
     body("password").trim()
         .isLength({ min: 3 }).withMessage(`Password ${passwordErr}`),
     body("confirmPassword").trim()
