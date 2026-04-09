5e82c5a581c92993a73a00697cc43f95efefb5dc
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-04-09 20:59:33 +0100

    folderValidator.js: Edited validateFolder so only constraint on title is that it does not include \/:*?"<>|  characters

--- validators/folderValidator.js
+++ validators/folderValidator.js
@@ -2,12 +2,12 @@ const { body, validationResult, matchedData } = require("express-validator");
 const db = require("../db/queries");
 
 const lengthErr = "cannot be blank";
-const nameErr = "can only contain letters and numbers";
+const nameErr = 'cannot use \/:*?"<>|';
 const nameInUseErr = "Name already in use";
 
 const validateFolder = [
     body("title").trim()
-        .matches(/^[a-zA-Z0-9]+$/, 'i').withMessage(`Name ${nameErr}`)
+        .matches(/^[^\\/:*?"<>|]+$/).withMessage(`Name ${nameErr}`)
         .isLength({ min: 1 }).withMessage(`Name ${lengthErr}`)
         .custom(async value => {
                     const folder = await db.findFolderByTitle({ title: value });
