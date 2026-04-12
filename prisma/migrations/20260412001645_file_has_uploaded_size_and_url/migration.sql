322b7fe07aad489866d632ff0f275b6cb27e0def
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-04-12 01:17:09 +0100

    Migrated latest schema

---
+++ prisma/migrations/20260412001645_file_has_uploaded_size_and_url/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `published` on the `File` table. All the data in the column will be lost.
+  - Added the required column `size` to the `File` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `url` to the `File` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "File" DROP COLUMN "published",
+ADD COLUMN     "size" TEXT NOT NULL,
+ADD COLUMN     "uploaded" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "url" TEXT NOT NULL;
