f020ac11ce7ff275be0a9bd096c607360b58bab8
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2026-04-23 19:17:17 +0100

    fileValidator.js: Added logic for storage destination and upload const with limit of 5mb. Need to add fileFilter for files that already exist

---
+++ validators/fileValidator.js
@@ -0,0 +1,18 @@
+const multer = require("multer");
+const path = require("path");
+const db = require("../db/queries");
+
+
+const storage = multer.diskStorage({
+    destination: './uploads',
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 5 * 1024 },
+});
+
+module.exports = upload;
